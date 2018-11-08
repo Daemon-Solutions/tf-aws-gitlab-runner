146ba66fd33014682b4d6f7586908eec709dd6f6
Russell Whelan <russell.whelan@claranet.uk>
2018-11-08 13:25:09 +0000

    Inital commit

---
+++ main.tf
@@ -0,0 +1,54 @@
+/**
+ * # tf-aws-gitlab-runner
+ * 
+ * A Gitlab Runner Terraform Module
+ *
+ * Give you:
+ * - An ASG for a single EC2 node with gitlab runner installed and running
+ *
+ * ## Contributing
+ *
+ * Ensure any variables you add have a type and a description.
+ * This README is generated with [terraform-docs](https://github.com/segmentio/terraform-docs):
+ *
+ * `terraform-docs --no-sort md . > README.md`
+ *
+*/
+data "template_file" "user_data" {
+  template = "${file("${path.module}/user-data.sh")}"
+  vars {
+    GITLAB_RUNNER_NAME                    = "${var.gitlab_runner_name}"
+    GITLAB_RUNNER_URL                     = "${var.gitlab_runner_url}"
+    GITLAB_RUNNER_TOKEN                   = "${var.gitlab_runner_token}"
+    GITLAB_RUNNER_TAGS                    = "${var.gitlab_runner_tags}"
+    GITLAB_RUNNER_DOCKER_IMAGE            = "${var.gitlab_runner_docker_image}"
+    GITLAB_CONCURRENT_JOB                 = "${var.gitlab_concurrent_job}"
+    GITLAB_CHECK_INTERVAL                 = "${var.gitlab_check_interval}"
+
+    GITLAB_RCT_LOW_FREE_SPACE             = "${var.gitlab_rct_low_free_space}"
+    GITLAB_RCT_EXPECTED_FREE_SPACE        = "${var.gitlab_rct_expected_free_space}"
+    GITLAB_RCT_LOW_FREE_FILES_COUNT       = "${var.gitlab_rct_low_free_files_count}"
+    GITLAB_RCT_EXPECTED_FREE_FILES_COUNT  = "${var.gitlab_rct_expected_free_files_count}"
+    GITLAB_RCT_DEFAULT_TTL                = "${var.gitlab_rct_default_ttl}"
+    GITLAB_RCT_USE_DF                     = "${var.gitlab_rct_use_df}"
+  }
+}
+
+module "app" {
+  source = "git.bashton.net/Bashton-Terraform-Modules/tf-aws-asg"
+
+  subnets              = ["${var.vpc_subnets}"]
+  name                 = "gitlab-runner-${var.vpc_env}"
+  service              = "gitlab-runner"
+  key_name             = "${var.key_name}"
+  instance_type        = "${var.instacne_size}"
+  envname              = "${var.vpc_env}"
+  ami_id               = "${var.ami_id}"
+  iam_instance_profile = "${var.iam_profile}"
+  security_groups      = ["${var.security_groups}"]
+  user_data            = "${data.template_file.app_userdata.rendered}"
+  min                  = "1"
+  max                  = "1"
+
+}
+
---
+++ variables.tf
@@ -0,0 +1,106 @@
+variable "vpc_subnets" {
+    description = "List of subnets that the GitLab runner will be placed in"
+    type = "list"
+}
+
+variable "vpc_env" {
+    description = "Envrioment name of VPC. I.e. Prod, Mgmt etc."
+    type = "string"
+}
+
+variable "key_name" {
+    description = "Name of the SSH key to use"
+    type = "string"
+}
+
+variable "ami_id" {
+    description = "ID of the base AMI the runner will use"
+    type = "string"
+}
+
+variable "iam_profile" {
+    description = "Resource ID of the IAM profile the instance will use"
+    type = "string"
+}
+
+variable "security_groups" {
+    description = "List of addtional security group to apply"
+    type = "list" 
+}
+
+variable "gitlab_runner_name" {
+    description = "Name that the runner will appear with in GitLab"
+    type = "string"
+}
+
+variable "gitlab_runner_url" {
+    description = "URL of the GitLab CI server to connect to"
+    type = "string"
+    default = "https://gitlab.com/ci"
+}
+
+variable "gitlab_runner_token" {
+    description = "Token used to authenticate with CI server"
+    type = "string"
+}
+
+variable "gitlab_runner_tags" {
+    description = "Tags to add to the runner"
+    type = "string"
+    default = "specific,docker"
+  
+}
+
+variable "gitlab_runner_docker_image" {
+    description = "Gitlab Runner default docker image."
+    type = "string"
+    default = "terraform:light"
+}
+
+variable "gitlab_concurrent_job" {
+    description = "Number of concurrent tasks to run"
+    type = "string"
+    default = "1"
+} 
+
+variable "gitlab_rct_low_free_space" {
+    description = "Threshold for when to trigger the cache and image removal"
+    type = "string"
+    default = "1GB"
+}
+
+variable "gitlab_rct_expected_free_space" {
+    description = "How much the free space to cleanup"
+    type = "string"
+    default = "2GB"
+}
+
+variable "gitlab_rct_low_free_file_space" {
+    description = "When the number of free files (i-nodes) runs below this value trigger the cache and image removal"
+    type = "string"
+    default = "2GB"
+}
+
+variable "gitlab_rct_low_free_file_space" {
+    description = "When the number of free files (i-nodes) runs below this value trigger the cache and image removal"
+    type = "string"
+    default = "131072"
+}
+
+variable "gitlab_rct_expected_free_file_space" {
+    description = "How many free files (i-nodes) to cleanup"
+    type = "string"
+    default = "262144"
+}
+
+variable "gitlab_rct_default_ttl" {
+    description = "Minimum time to preserve a newly downloaded images or created caches"
+    type = "string"
+    default = "1m"
+}
+
+variable "gitlab_rct_use_df" {
+    description = "Use a command line df tool to check disk space. Set to false when connecting to remote Docker Engine. Set to true when using with locally installed Docker Engine"
+    type = "string"
+    default = "1"
+}
